0e478989e6bd6ab3e62e700911907d3faa5321b6
andremlsantos <andremlsantos@gmail.com>
2020-04-29 15:21:18 +0200

    add eadme updtd header links
    add eadme updtd header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,33 @@
-import React, { Component } from 'react'
-import '../css/Header.css'
+import React, { Component } from "react";
+import "../css/Header.css";
 
 export class Header extends Component {
-    render() {
-        return (
-            <nav>
-                <ul>
-                    <li><a href="https://www.secfi.com/" className="left logo"> </a></li>
-                    <li><a href="#" className="right">Contact</a></li>
-                    <li><a href="#" className="right">GitHub</a></li>
-                </ul>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.secfi.com/" className="left logo">
+              {" "}
+            </a>
+          </li>
+          <li>
+            <a href="mailto:andremlsantos@protonmail.com" className="right">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/andremlsantos/secfi-front-end-typescript.git"
+              className="right"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
-export default Header
+export default Header;
